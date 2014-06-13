f69124fe170c3ce0ba7893324895fa61f20582da
Pierre-Yves Lejeune <lejeune.py@gmail.com>
2014-06-13 17:21:19 +0200

    updated init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$(id -u)" != "0" ]; then
-echo "This script must be run as root" 1>&2
-exit 1
-fi
+#if [ "$(id -u)" != "0" ]; then
+#echo "This script must be run as root" 1>&2
+#exit 1
+#fi
 
 vagrant up
 echo "192.168.121.100    convert-nl.local" >>  /etc/hosts
